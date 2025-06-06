e2c8c19eeaa13dec4055bb888bf32a5fae8cd776
Paras Goel <parasgoel@Parass-MacBook-Pro.local>
2025-06-05 22:44:29 -0700

    update 3

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
 import os
 
-app = Flask(__name__, static_folder='../frontend', static_url_path='')
+app = Flask(__name__, static_folder='frontend', static_url_path='')
 CORS(app)
 
 # Serve index.html when visiting the root

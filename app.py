d6c6c123da921b26c018c3101fd9160a043efdf1
Paras Goel <87245548+parser06@users.noreply.github.com>
2025-06-06 00:54:17 -0400

    Update app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Flask on Render!"
+
+@app.route('/api/charities')
+def charities():
+    return jsonify([
+        {"name": "UNICEF", "url": "https://www.unicef.org"},
+        {"name": "Red Cross", "url": "https://www.redcross.org"},
+        {"name": "Charity: Water", "url": "https://www.charitywater.org"},
+    ])
+
+if __name__ == '__main__':
+    app.run()

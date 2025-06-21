5ed9767390fd8da3dc7f5885525fc9d6a641dfdd
Paras Goel <parasgoel@Parass-MacBook-Pro.local>
2025-06-21 13:30:44 -0700

    mongodb

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify, send_from_directory, request
 from flask_cors import CORS
+from pymongo import MongoClient
 import os
-from flask import request
 import uuid
 
 app = Flask(__name__, static_folder='frontend', static_url_path='')
 CORS(app)
+
+MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://charity_user:your_password_here@cluster0.8swpuom.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')  # replace if not using env
+client = MongoClient(MONGO_URI)
+db = client['charity_db']
+charity_collection = db['charities']
 
 # Serve index.html when visiting the root
 @app.route('/')
 def serve_index():
     return send_from_directory(app.static_folder, 'index.html')
 
+
+# Get data from MongoDB
+@app.route('/api/charities')
+def get_charities():
+    charities = list(charity_collection.find({}, {'_id': 0}))  # exclude MongoDB's _id field
+    return jsonify(charities)
+
+'''
 # Serve the charities API
 @app.route('/api/charities')
 def get_charities():
@@ -21,6 +34,7 @@ def get_charities():
         {"name": "Charity: Water", "url": "https://www.charitywater.org"},
         {"name": "Doctors Without Borders", "url": "https://www.doctorswithoutborders.org/"},
     ])
+'''
 
 @app.route('/api/donate', methods=['POST'])
 def donate():
@@ -46,27 +60,3 @@ def serve_static(path):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
-
-'''
-from flask import Flask, jsonify
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/')
-def home():
-    return "Hello from Flask on Render!"
-
-@app.route('/api/charities')
-def charities():
-    return jsonify([
-        {"name": "UNICEF", "url": "https://www.unicef.org"},
-        {"name": "Red Cross", "url": "https://www.redcross.org"},
-        {"name": "Charity: Water", "url": "https://www.charitywater.org"},
-    ])
-
-if __name__ == '__main__':
-    app.run()
-'''
